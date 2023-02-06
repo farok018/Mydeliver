e070fea918584a46f1cd590fdbd1a67fdb996072
hairul <hairul@localhost>
2023-02-05 23:13:51 -0600

    i'm uploading my first work

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hairul
+ *
+ */
+module Mydeliver {
+}
---
+++ src/practice/welcome.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class welcome {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to DevOps Training");
+		System.out.println(2+2);
+		System.out.println("By Farok");
+		
+		
+
+	}
+
+}
