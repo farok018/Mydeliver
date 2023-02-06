09a280b749cae46d70949734034a4df33059b3d8
hairul <hairul@localhost>
2023-02-05 23:19:48 -0600

    successfully did

--- src/practice/welcome.java
+++ src/practice/welcome.java
@@ -7,6 +7,7 @@ public class welcome {
 		System.out.println("Welcome to DevOps Training");
 		System.out.println(2+2);
 		System.out.println("By Farok");
+		System.out.println("Done work");
 		
 		
 		
